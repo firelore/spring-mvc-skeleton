25a67ee52ed16d0b273bc8d10e13f44871d8d1ec
Mike Spencer <firelore@gmail.com>
2013-07-28 11:49:23 -0500

    Add secondary route for our home page
    Sometimes we may need to specify a secondary URL to reach the same page
    at. Spring lets us specify multiple URLs for the same handler method via
    the 'value' attribute of @RequestMapping. Simply pass in an array of URLs
    you need to reach the handler through.

--- src/main/java/com/github/firelore/controller/IndexController.java
+++ src/main/java/com/github/firelore/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	@RequestMapping("/")
+	@RequestMapping(value = {"/", "/index"})
 	public String index() {
 		return "index";
 	}
