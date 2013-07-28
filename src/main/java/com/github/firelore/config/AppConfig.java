0bb3bd07ab324aacc586047d884f88c1b941ac2b
Mike Spencer <firelore@gmail.com>
2013-07-28 11:11:10 -0500

    Add Spring configuration files (as javaconfig)

---
+++ src/main/java/com/github/firelore/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.github.firelore.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/com/github/firelore/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.github.firelore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.github.firelore.controllers"})
+public class WebConfig {
+	@Bean
+	public ViewResolver viewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+}
